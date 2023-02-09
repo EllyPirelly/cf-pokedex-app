7e4955999b95b2ccda7ffdb24a77d6fcc56cef4a
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-02-09 18:34:08 +0100

    add IIFE, getAll function, add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,48 @@
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: [
-      'grass',
-      'poison'
-    ]
-  },
-  {
-    name: 'Jigglypuff',
-    height: 0.5,
-    types: [
-      'fairy',
-      'normal'
-    ]
-  },
-  {
-    name: 'Tentacruel',
-    height: 1.6,
-    types: [
-      'water',
-      'poison'
-    ]
+var pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: [
+        'grass',
+        'poison'
+      ]
+    },
+    {
+      name: 'Jigglypuff',
+      height: 0.5,
+      types: [
+        'fairy',
+        'normal'
+      ]
+    },
+    {
+      name: 'Tentacruel',
+      height: 1.6,
+      types: [
+        'water',
+        'poison'
+      ]
+    }
+  ];
+
+  // return all items
+  function getAll(pokemonRepository) {
+    return pokemonList;
   }
-];
+
+  // add single item to pokemonList array
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    getAll,
+    add
+  }
+
+
+})();
 
 pokemonList.forEach(function(item) {
   if (item.height > 1.0) {
@@ -32,19 +51,3 @@ pokemonList.forEach(function(item) {
     document.write('<div class="list-item">Name: ' + item.name + ' (height: ' + item.height + ' m)</div>');
   }
 });
-
-// old for Loop
-// for (i = 0; i < pokemonList.length; i++) {
-//   let pokeName = pokemonList[i].name;
-//   let pokeHeight = pokemonList[i].height;
-
-//   if (pokeHeight > 1.0) {
-//     document.write(`
-//       <div class='list-item'>${pokeName} (height: ${pokeHeight} m) - Wow, that's big!</div>
-//     `);
-//   } else {
-//     document.write(`
-//       <div class='list-item'>${pokeName} (height: ${pokeHeight} m)</div>
-//     `);
-//   }
-// }
