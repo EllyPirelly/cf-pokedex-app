409449f58f3cb376d4aec90ca2caf5f2d0e7a075
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-02-15 08:53:20 +0100

    get API data (all and detail)

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,24 @@
-var pokemonRepository = (function () {
-  let pokemonList = [
-    {
-      name: 'Bulbasaur',
-      height: 0.7,
-      types: [
-        'grass',
-        'poison'
-      ]
-    },
-    {
-      name: 'Jigglypuff',
-      height: 0.5,
-      types: [
-        'fairy',
-        'normal'
-      ]
-    },
-    {
-      name: 'Tentacruel',
-      height: 1.6,
-      types: [
-        'water',
-        'poison'
-      ]
-    }
-  ];
-
-  // get and return all items of pokemonList array
-  function getAll() {
-    return pokemonList;
-  };
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   // check type and push single item to pokemonList array
   function add(item) {
     if (
-      typeof item === 'object' &&
-      'name' in item &&
-      'height' in item &&
-      'types' in item
+      typeof item === 'object' //&&
+      // 'name' in item &&
+      // 'height' in item &&
+      // 'types' in item
     ) {
       pokemonList.push(item);
     } else {
       console.log('Nope, not a valid Poke.');
     }
+  };
+
+  // get and return all items of pokemonList array
+  function getAll() {
+    return pokemonList;
   };
 
   // create and append
@@ -66,11 +42,6 @@ var pokemonRepository = (function () {
     listItem.appendChild(itemButton);
   };
 
-  // console.log pokemon details at button click
-  function showDetails(pokemon) {
-    console.log(pokemon);
-  };
-
   // add event handler on button with "outer" function
   function addEl (itemButton, pokemon) {
     if (itemButton) {
@@ -83,30 +54,74 @@ var pokemonRepository = (function () {
     }
   };
 
+  // fetch data from the API
+  // add fetched items to pokemonList
+  // use detailsUrl to load detailed data
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (event) {
+      console.error(event);
+    });
+  };
+
+  // fetch details
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (event) {
+      console.error(event);
+    });
+  };
+
+  // console.log pokemon details at button click
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  };
+
   return {
     add,
     getAll,
-    addListItem
+    addListItem,
+    loadList,
+    loadDetails,
+    showDetails
   };
 
 })();
 
 // TODO: delete when "real" data comes in
-pokemonRepository.add({
-  name: 'testpoke',
-  height: 0.5,
-  types: [
-    'evil',
-    'sunny'
-  ]
-});
+// pokemonRepository.add({
+//   name: 'testpoke',
+//   height: 0.5,
+//   types: [
+//     'evil',
+//     'sunny'
+//   ]
+// });
 
 // call getAll on pokemonRepository
 // run forEach on pokemonRepository
 // run addListItem in each iteration
-pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
-
-// or this:
-// pokemonRepository.getAll().forEach(function(item) {
-//   pokemonRepository.addListItem(item);
-// });
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
