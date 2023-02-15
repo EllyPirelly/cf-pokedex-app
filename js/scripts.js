8a29ae435f37e3bb01c4a18d3ad17333fc798287
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-02-15 16:21:12 +0100

    add basic modal from exercise

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ let pokemonRepository = (function () {
   let pokemonList = [];
   // TODO: change back to 150 items when necessary
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=15';
+  let modalContainer = document.querySelector('#modal-container');
 
   // typeof check on what's given back by API
   // push single item to pokemonList array
@@ -89,6 +90,61 @@ let pokemonRepository = (function () {
       console.error(event);
     });
   };
+
+  // ADD EXERCISE MODAL HERE
+  // TODO: figure out how to hand in an image
+  function showModal(name, height, img) {
+
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let nameElement = document.createElement('div');
+    nameElement.innerText = name;
+
+    let heightElement = document.createElement('div');
+    heightElement.innerText = height;
+
+    let imageElement = document.createElement('img');
+    imageElement.setAttribute('src', img);
+    imageElement.setAttribute('width', '100');
+    imageElement.setAttribute('height', '100');
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(nameElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  };
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  };
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('todo pokename', 'todo pokeheight', 'todo pokeimage');
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
   // console.log pokemon details at button click
   function showDetails(item) {
