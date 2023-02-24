b37233515aaa6e3097fdae76a34e34517553cd72
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-02-24 12:55:28 +0100

    integrate eslint, fix errors

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,9 @@
-let pokemonRepository = (function () {
+/* eslint-disable func-names */
+const pokemonRepository = (function () {
   // empty array for incoming data
-  let pokemonList = [];
+  const pokemonList = [];
   // change limit to more items in case more are necessary
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=30';
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=30';
 
   // typeof check on what's given back by API
   // push valid single item to pokemonList array
@@ -21,14 +22,99 @@ let pokemonRepository = (function () {
     return pokemonList;
   }
 
+  // show Bootstrap modal; (only here) jQuery is used for the exercise
+  function showModal(item) {
+    pokemonRepository.loadDetails(item).then(() => {
+      // grab modal-content to empty it
+      const modalContent = $('.modal-content');
+      // grab modal header, modal body
+      const modalHeader = $('.modal-header');
+      const modalBody = $('.modal-body');
+
+      // empty modal header, modal body
+      modalHeader.empty();
+      modalBody.empty();
+
+      modalContent.val('');
+
+      // create elements inside modal header
+      const closeButtonElement = $(
+        `<button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>`
+      );
+      const ariaButton = $(`<span aria-hidden="true">X</span>`);
+
+      // create elements inside modal body
+      const pokeIllu = $(`<div class="modal-body__img-wrapper"></div>`);
+      const pokeLogo = $(`<img class="modal-header__logo">`).attr(
+        'src',
+        'src/img/svg-pokemon-logo.png'
+      );
+      const pokeImage = $(`<img class="modal-item__img" alt="">`).attr(
+        'src',
+        item.imageUrl
+      );
+
+      const pokeDetails = $(`<div class="modal-body__details-wrapper"></div>`);
+      const pokeName = $(
+        `<h2 class="modal-body__headline headline-secondary">${item.name}</h2>`
+      );
+      const pokeHeight = $(
+        `<p class="modal-body__item--height"><b>Height: </b>${item.height}</p>`
+      );
+
+      // loop over pokeTypes
+      let pokeTypes = '';
+
+      for (let i = 0; i < item.types.length; i += 1) {
+        pokeTypes += item.types[i].type.name;
+        if (i + 1 !== item.types.length) {
+          pokeTypes += ', ';
+        }
+      }
+
+      // create element for pokeTypes
+      const typesElement = $(
+        `<p class="modal-body__item--types"><b>Types: </b>${pokeTypes}</p>`
+      );
+
+      // append all
+      modalHeader.append(pokeLogo);
+      modalHeader.append(closeButtonElement);
+      closeButtonElement.append(ariaButton);
+
+      modalBody.append(pokeIllu);
+      modalBody.append(pokeDetails);
+      pokeIllu.append(pokeImage);
+      pokeDetails.append(pokeName);
+      pokeDetails.append(pokeHeight);
+      pokeDetails.append(typesElement);
+    });
+  }
+
+  // event handler on button
+  function clickForDetails(itemButton, item) {
+    if (itemButton) {
+      itemButton.addEventListener('click', () => {
+        showModal(item);
+      });
+      /* eslint-disable no-console */
+      console.log('yes, i work');
+      /* eslint-disable no-console */
+    } else {
+      /* eslint-disable no-console */
+      console.log('nope, somethings off');
+      /* eslint-disable no-console */
+    }
+  }
+
   // create and append elements
   function addListItem(item) {
     // grab outer ul element
-    let list = document.querySelector('.list-item-wrapper');
+    const list = document.querySelector('.list-item-wrapper');
 
     // create li and button elements
-    let listItem = document.createElement('li');
-    let itemButton = document.createElement('button');
+    const listItem = document.createElement('li');
+    const itemButton = document.createElement('button');
 
     // add class to li
     listItem.classList.add('group-list-item');
@@ -51,40 +137,22 @@ let pokemonRepository = (function () {
     clickForDetails(itemButton, item);
   }
 
-  // event handler on button
-  function clickForDetails(itemButton, item) {
-    if (itemButton) {
-      itemButton.addEventListener('click', function () {
-        showModal(item);
-      });
-      /* eslint-disable no-console */
-      console.log('yes, i work');
-      /* eslint-disable no-console */
-    } else {
-      /* eslint-disable no-console */
-      console.log('nope, somethings off');
-      /* eslint-disable no-console */
-    }
-  }
-
   // fetch data from the API
   // add fetched items to pokemonList
   // use detailsUrl to load detailed data
   function loadList() {
     return fetch(apiUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (json) {
-        json.results.forEach(function (item) {
-          let pokemon = {
+      .then((response) => response.json())
+      .then((json) => {
+        json.results.forEach((item) => {
+          const pokemon = {
             name: item.name,
             detailsUrl: item.url,
           };
           add(pokemon);
         });
       })
-      .catch(function (event) {
+      .catch((event) => {
         /* eslint-disable no-console */
         console.error(event);
         /* eslint-disable no-console */
@@ -92,100 +160,22 @@ let pokemonRepository = (function () {
   }
 
   // fetch details
+  /* eslint-disable no-param-reassign */
   function loadDetails(item) {
-    let url = item.detailsUrl;
+    const url = item.detailsUrl;
 
     return fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (details) {
+      .then((response) => response.json())
+      .then((details) => {
         item.imageUrl = details.sprites.other.dream_world.front_default;
         item.height = details.height;
         item.types = details.types;
       })
-      .catch(function (event) {
+      .catch((event) => {
         /* eslint-disable no-console */
         console.error(event);
         /* eslint-disable no-console */
       });
-  }
-
-  // show Bootstrap modal; (only here) jQuery is used for the exercise
-  function showModal(item) {
-    pokemonRepository.loadDetails(item).then(function () {
-      // grab modal-content to empty it
-      let modalContent = $('.modal-content');
-      // grab modal header, modal body
-      let modalHeader = $('.modal-header');
-      let modalBody = $('.modal-body');
-
-      // empty modal header, modal body
-      modalHeader.empty();
-      modalBody.empty();
-
-      modalContent.val('');
-
-      // create elements inside modal header
-      let closeButtonElement = $(
-        '<button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>'
-      );
-      let ariaButton = $('<span aria-hidden="true">' + 'X' + '</span>');
-
-      // create elements inside modal body
-      let pokeIllu = $('<div class="modal-body__img-wrapper"></div>');
-      let pokeLogo = $('<img class="modal-header__logo">').attr(
-        'src',
-        'src/img/svg-pokemon-logo.png'
-      );
-      let pokeImage = $('<img class="modal-item__img" alt="">').attr(
-        'src',
-        item.imageUrl
-      );
-
-      let pokeDetails = $('<div class="modal-body__details-wrapper"></div>');
-      let pokeName = $(
-        '<h2 class="modal-body__headline headline-secondary">' +
-          item.name +
-          '</h2>'
-      );
-      let pokeHeight = $(
-        '<p class="modal-body__item--height">' +
-          '<b>Height: </b>' +
-          item.height +
-          '</p>'
-      );
-
-      // loop over pokeTypes
-      let pokeTypes = '';
-
-      for (let i = 0; i < item.types.length; i++) {
-        pokeTypes += item.types[i].type.name;
-        if (i + 1 !== item.types.length) {
-          pokeTypes += ', ';
-        }
-      }
-
-      // create element for pokeTypes
-      let typesElement = $(
-        '<p class="modal-body__item--types">' +
-          '<b>Types: </b>' +
-          pokeTypes +
-          '</p>'
-      );
-
-      // append all
-      modalHeader.append(pokeLogo);
-      modalHeader.append(closeButtonElement);
-      closeButtonElement.append(ariaButton);
-
-      modalBody.append(pokeIllu);
-      modalBody.append(pokeDetails);
-      pokeIllu.append(pokeImage);
-      pokeDetails.append(pokeName);
-      pokeDetails.append(pokeHeight);
-      pokeDetails.append(typesElement);
-    });
   }
 
   return {
@@ -201,8 +191,8 @@ let pokemonRepository = (function () {
 // loadList
 // if success, getAll which is the pokemonList
 // for each item, add item to pokemonList
-pokemonRepository.loadList().then(function () {
-  pokemonRepository.getAll().forEach(function (pokemon) {
+pokemonRepository.loadList().then(() => {
+  pokemonRepository.getAll().forEach((pokemon) => {
     pokemonRepository.addListItem(pokemon);
   });
 });
